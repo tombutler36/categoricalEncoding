120b6846c36a568cfbeb9dc68a12f9db7880f0f9
tombutler36 <57366547+tombutler36@users.noreply.github.com>
2021-02-09 21:43:42 -0800

    Add files via upload

---
+++ Learned Embedding Amazon.py
@@ -0,0 +1,95 @@
+from numpy import unique
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from keras.models import Model
+from keras.layers import Input
+from keras.layers import Dense
+from keras.layers import Embedding
+from keras.layers.merge import concatenate
+
+
+# load the dataset
+def load_dataset(filename):
+    # retrieve numpy array
+    dataset = filename.values
+    # split into input (X) and output (y) variables
+    X = dataset[:, :-1]
+    y = dataset[:, -1]
+    # format all fields as string
+    X = X.astype(str)
+    # reshape target to be a 2d array
+    y = y.reshape((len(y), 1))
+    return X, y
+
+
+# prepare input data
+def prepare_inputs(X_train, X_test):
+    X_train_enc, X_test_enc = list(), list()
+    # label encode each column
+    for i in range(X_train.shape[1]):
+        le = LabelEncoder()
+        # encode
+        train_enc = le.fit_transform(X_train[:, i])
+        test_enc = le.fit_transform(X_test[:, i])
+        # store
+        X_train_enc.append(train_enc)
+        X_test_enc.append(test_enc)
+    return X_train_enc, X_test_enc
+
+
+# prepare target
+def prepare_targets(y_train, y_test):
+    le = LabelEncoder()
+    le.fit(y_train)
+    y_train_enc = le.transform(y_train)
+    y_test_enc = le.transform(y_test)
+    return y_train_enc, y_test_enc
+
+x1 = pd.read_csv('x_train_amz.csv', header=None)
+x2 = pd.read_csv('x_test_amz.csv', header=None)
+y1 = pd.read_csv('y_train_amz.csv', header=None)
+y2 = pd.read_csv('y_test_amz.csv', header=None)
+x1 = x1.astype(str)
+x2 = x2.astype(str)
+xcat = x1.append(x2, ignore_index=True)
+ytot = y1.append(y2, ignore_index=True)
+total_data = pd.concat((xcat, ytot), axis=1, ignore_index=True)
+
+# load the dataset
+X, y = load_dataset(total_data)
+# split into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=1)
+# prepare input data
+X_train_enc, X_test_enc = prepare_inputs(X_train, X_test)
+# prepare output data
+y_train_enc, y_test_enc = prepare_targets(y_train, y_test)
+# make output 3d
+y_train_enc = y_train_enc.reshape((len(y_train_enc), 1, 1))
+y_test_enc = y_test_enc.reshape((len(y_test_enc), 1, 1))
+# prepare each input head
+in_layers = list()
+em_layers = list()
+for i in range(len(X_train_enc)):
+    # calculate the number of unique inputs
+    n_labels = len(unique(X_train_enc[i]))
+    # define input layer
+    in_layer = Input(shape=(1,))
+    # define embedding layer
+    em_layer = Embedding(n_labels, 10)(in_layer)
+    # store layers
+    in_layers.append(in_layer)
+    em_layers.append(em_layer)
+# concat all embeddings
+merge = concatenate(em_layers)
+dense = Dense(10, activation='relu', kernel_initializer='he_normal')(merge)
+output = Dense(1, activation='sigmoid')(dense)
+model = Model(inputs=in_layers, outputs=output)
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# plot graph
+# fit the keras model on the dataset
+model.fit(X_train_enc, y_train_enc, epochs=20, batch_size=16, verbose=2)
+# evaluate the keras model
+_, accuracy = model.evaluate(X_test_enc, y_test_enc, verbose=0)
+print('Accuracy: %.2f' % (accuracy * 100))
